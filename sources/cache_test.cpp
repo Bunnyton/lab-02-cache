533acb4f6b96f75bc324f411297294a438e9d84e
Bunnyton <galaktionovaa@student.bmstu.ru>
2021-12-12 16:50:25 +0300

    style correction

--- sources/cache_test.cpp
+++ sources/cache_test.cpp
@@ -1,13 +1,14 @@
+// Copyright 2021 Galaktionov Andrey <galaktionovaa@student.bmstu.ru>
+
 #include "cache_test.hpp"
 
 #include <random>
 
-std::vector<unsigned> buffer_size (std::vector<unsigned>& cache_size)
-{
+std::vector<unsigned> buffer_size(std::vector<unsigned>& cache_size) {
   std::vector<unsigned> buff_size;
 
   unsigned helper = cache_size.front() / 2;
-  while (helper < 3 * cache_size.back() / 2 ) {
+  while (helper < 3 * cache_size.back() / 2) {
     buff_size.push_back(helper);
     helper *= 2;
   }
@@ -17,7 +18,8 @@ std::vector<unsigned> buffer_size (std::vector<unsigned>& cache_size)
 unsigned* set_of_test(unsigned buff_size, move_type t, unsigned lot_of_iter) {
   if (buff_size == 0) return nullptr;
 
-  unsigned lot_of_elements = (buff_size * K / sizeof(unsigned)) / STEP;//buff size in kib
+  unsigned lot_of_elements =
+      (buff_size * K / sizeof(unsigned)) / STEP;  // buff size in kib
   if (lot_of_elements == 0) return nullptr;
 
   auto* set_of_elements = new unsigned[lot_of_elements];
@@ -25,8 +27,7 @@ unsigned* set_of_test(unsigned buff_size, move_type t, unsigned lot_of_iter) {
 
   unsigned helper = 0;
 
-  if (t == right_type)
-  {
+  if (t == right_type) {
     for (unsigned i = 0; i < lot_of_elements; ++i) {
       set_of_elements[i] = helper;
       helper += STEP;
@@ -47,47 +48,47 @@ unsigned* set_of_test(unsigned buff_size, move_type t, unsigned lot_of_iter) {
 
   if (t == random_type)
     std::shuffle(test_arr, test_arr - 1 + lot_of_iter,
-                                   std::mt19937(std::random_device()()));
+                 std::mt19937(std::random_device()()));
 
   delete[] set_of_elements;
   return test_arr;
 }
 
 unsigned* make_array(unsigned buff_size) {
-  auto* arr = new unsigned[buff_size * K / sizeof(unsigned)];//buff size in kib
+  auto* arr =
+      new unsigned[buff_size * K / sizeof(unsigned)];  // buff size in kib
   return arr;
 }
 
-void result_out(move_type t, unsigned number, std::vector<unsigned>& buffer_size
-                                       , clock_t duration
-                                       , std::ostream& os) {
+void result_out(move_type t, unsigned number,
+                std::vector<unsigned>& buffer_size, clock_t duration,
+                std::ostream& os) {
   if (number == 0) {
-    os << "investigation:" << std::endl
-       << "  travel_variant: ";
+    os << "investigation:" << std::endl << "  travel_variant: ";
     switch (t) {
-        case right_type:
-          os << "right";
-          break;
-        case reverse_type:
-          os << "reverse";
-          break;
-        case random_type:
-          os << "random";
-      }
+      case right_type:
+        os << "right";
+        break;
+      case reverse_type:
+        os << "reverse";
+        break;
+      case random_type:
+        os << "random";
+    }
     os << std::endl << "  experiments:" << std::endl;
   }
   os << "  - experiment:" << std::endl
-     << "      number: " <<  number + 1 << std::endl
+     << "      number: " << number + 1 << std::endl
      << "      input_data:" << std::endl
      << "        buffer_size: " << buffer_size.at(number) << "kib" << std::endl
      << "      results:" << std::endl
      << "        duration: " << duration * 1000000000 / CLOCKS_PER_SEC << "ns"
-     << std::endl << std::endl;
+     << std::endl
+     << std::endl;
 }
 
-void cache_test(std::vector<unsigned>& cache_size , unsigned lot_of_iter
-                                                           , std::ostream& os) {
-
+void cache_test(std::vector<unsigned>& cache_size, unsigned lot_of_iter,
+                std::ostream& os) {
   std::vector<unsigned> buff_size = buffer_size(cache_size);
 
   [[maybe_unused]] unsigned helper = 0;
@@ -95,10 +96,10 @@ void cache_test(std::vector<unsigned>& cache_size , unsigned lot_of_iter
   clock_t end = 0;
   for (unsigned i = 0; i < LOT_OF_TYPE; ++i) {
     for (unsigned j = 0; j < buff_size.size(); ++j) {
-
-      [[maybe_unused]] unsigned* set = set_of_test(buff_size.at(j), (move_type) i, lot_of_iter);
+      [[maybe_unused]] unsigned* set =
+          set_of_test(buff_size.at(j), (move_type)i, lot_of_iter);
       if (set == nullptr) {
-        result_out((move_type) i, j, buff_size, 0, os);
+        result_out((move_type)i, j, buff_size, 0, os);
         continue;
       }
       [[maybe_unused]] unsigned* arr = make_array(buff_size.at(j));
@@ -108,11 +109,10 @@ void cache_test(std::vector<unsigned>& cache_size , unsigned lot_of_iter
         helper = arr[set[k]];  // warm up
 
       start = clock();
-      for (unsigned k = 0; k < lot_of_iter; ++k)
-        helper = arr[set[k]];
+      for (unsigned k = 0; k < lot_of_iter; ++k) helper = arr[set[k]];
       end = clock();
 
-      result_out((move_type) i, j, buff_size, end - start, os);
+      result_out((move_type)i, j, buff_size, end - start, os);
 
       delete[] set;
       delete[] arr;
