bdb85d35ab9d14948f134d38e6d7a0036b51c85b
Bunnyton <galaktionovaa@student.bmstu.ru>
2021-12-12 17:10:49 +0300

    Try to delete errors

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 Galaktionov Andrey <galaktionovaa@student.bmstu.ru>
 
 #include <stdexcept>
-
 #include <gtest/gtest.h>
 
-#include <example.hpp>
+#include "cache_test.hpp"
 
 TEST(Example, EmptyTest) {
     EXPECT_THROW(example(), std::runtime_error);
